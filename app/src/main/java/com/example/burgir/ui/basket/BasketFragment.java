00b45c9ea8d468d84e61440e7ac4572373e19c3b
ismael argence <ismaelargence3@gmail.com>
2023-02-24 12:25:05 +0100

    debut de la barre de navigation

---
+++ app/src/main/java/com/example/burgir/ui/basket/BasketFragment.java
@@ -0,0 +1,35 @@
+package com.example.burgir.ui.basket;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.burgir.databinding.FragmentDashboardBinding;
+
+public class BasketFragment extends Fragment {
+
+private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        BasketViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(BasketViewModel.class);
+
+    binding = FragmentDashboardBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
