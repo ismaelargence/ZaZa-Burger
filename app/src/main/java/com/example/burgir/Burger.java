c5db9f29569f99873b017aae887d149850df30be
Hugo <101125294+Lony027@users.noreply.github.com>
2023-03-21 14:29:03 +0100

    init + ajout pojo

---
+++ app/src/main/java/com/example/burgir/Burger.java
@@ -0,0 +1,15 @@
+package com.example.burgir;
+
+import java.util.ArrayList;
+
+public class Burger {
+
+    private int idBurger;
+    private String nomBurger;
+    private String description;
+    private ArrayList<Ingredient> ingredients;
+
+
+}
+
+
---
+++ app/src/main/java/com/example/burgir/Ingredient.java
@@ -0,0 +1,10 @@
+package com.example.burgir;
+
+public class Ingredient {
+
+    private int idIngredient;
+    private String nomIngredient;
+    private float prix;
+    private int stock;
+    private String typeIngredients;
+}
